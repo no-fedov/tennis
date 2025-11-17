ec24aeac55bb000ea205a1b2ec3dcefa785f0b6b
no-fedov <nefedov.evgeniy.d@yandex.com>
2025-11-17 18:12:57 +0300

    feat: add match

--- src/main/java/org/tennis/model/Match.java
+++ src/main/java/org/tennis/model/Match.java
@@ -1,33 +1,43 @@
 package org.tennis.model;
 
+import lombok.Getter;
 
 import java.util.LinkedList;
 import java.util.List;
 
+@Getter
 public class Match {
 
-    Integer winner;
-    boolean isComplete;
+    private static final int MIN_SETS_COUNT = 2;
 
+    private Participant winner;
+    private boolean isComplete;
     private final List<Set> sets = new LinkedList<>();
 
-    public void playMatch(int playerNumber) {
+    public void play(Participant pointWinner) {
         if (isComplete) {
             return;
         }
         if (sets.isEmpty()) {
+            // TODO: инвертировать зависимость
             sets.add(new Set());
         }
-        Set lastSet = getLastOrInitSet();
-        // TODO: сделать логику игры
-    }
-
-    private Set getLastOrInitSet() {
         Set lastSet = sets.getLast();
-        if (sets.isEmpty() || lastSet.isComplete()) {
+        lastSet.play(winner);
+
+        if (isLastPoint(pointWinner)) {
+            this.winner = pointWinner;
+            isComplete = true;
+            return;
+        }
+
+        if (lastSet.isComplete()) {
             sets.add(new Set());
         }
-        return sets.getLast();
     }
 
+    private boolean isLastPoint(Participant pointWinner) {
+        return sets.size() >= MIN_SETS_COUNT
+                && sets.stream().filter(set -> set.getWinner() == pointWinner).count() > 1;
+    }
 }
