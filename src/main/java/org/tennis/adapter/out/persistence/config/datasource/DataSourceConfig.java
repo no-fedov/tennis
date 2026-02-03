b46a34759c58cde6fad6b6f2ff8b31b76d4a736b
no-fedov <nefedov.evgeniy.d@yandex.com>
2026-02-03 16:41:34 +0300

    ref: fix init properties

--- src/main/java/org/tennis/adapter/out/persistence/config/datasource/DataSourceConfig.java
+++ src/main/java/org/tennis/adapter/out/persistence/config/datasource/DataSourceConfig.java
@@ -20,7 +20,23 @@ public class DataSourceConfig {
     public static final String HIBERNATE_HBM_2_DDL_AUTO = "hibernate.hbm2ddl.auto";
     private static final Properties properties = new Properties();
 
-    static {
+    private final EntityManagerFactory emf;
+
+    private DataSourceConfig() {
+        init();
+        final PersistenceConfiguration cfg = new PersistenceConfiguration("emf")
+                .property(JDBC_DRIVER, properties.getProperty(JDBC_DRIVER))
+                .property(JDBC_URL, properties.getProperty(JDBC_URL))
+                .property(JDBC_USER, properties.getProperty(JDBC_USER))
+                .property(JDBC_PASSWORD, properties.getProperty(JDBC_PASSWORD))
+                .property(SHOW_SQL, properties.getProperty(SHOW_SQL))
+                .property(HIBERNATE_HBM_2_DDL_AUTO, properties.getProperty(HIBERNATE_HBM_2_DDL_AUTO))
+                .managedClass(PlayerEntity.class)
+                .managedClass(MatchEntity.class);
+        emf = cfg.createEntityManagerFactory();
+    }
+
+    private static void init() {
         try (InputStream is = DataSourceConfig.class.getClassLoader().getResourceAsStream("hibernate.properties")) {
             if (is == null) {
                 throw new RuntimeException("Файл hibernate.properties не найден в classpath");
@@ -29,21 +45,7 @@ public class DataSourceConfig {
         } catch (IOException e) {
             throw new RuntimeException("Не удалось загрузить файл hibernate.properties", e);
         }
-    }
 
-    private final EntityManagerFactory emf;
-
-    private DataSourceConfig() {
-        final PersistenceConfiguration cfg = new PersistenceConfiguration("emf")
-                .property(JDBC_DRIVER, properties.getProperty(JDBC_DRIVER))
-                .property(JDBC_URL, properties.getProperty(JDBC_URL))
-                .property(JDBC_USER, properties.getProperty(JDBC_USER))
-                .property(JDBC_PASSWORD, properties.getProperty(JDBC_PASSWORD))
-                .property(SHOW_SQL, properties.getProperty(SHOW_SQL))
-                .property(HIBERNATE_HBM_2_DDL_AUTO,  properties.getProperty(HIBERNATE_HBM_2_DDL_AUTO))
-                .managedClass(PlayerEntity.class)
-                .managedClass(MatchEntity.class);
-        emf = cfg.createEntityManagerFactory();
     }
 
     private static class SingletonHolder {
