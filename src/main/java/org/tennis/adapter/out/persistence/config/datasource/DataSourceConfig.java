b4d1438291dce3fecc5933a1f399835fcc0f30f0
no-fedov <nefedov.evgeniy.d@yandex.com>
2026-02-03 16:29:10 +0300

    ref: take out configurations to resources

--- src/main/java/org/tennis/adapter/out/persistence/config/datasource/DataSourceConfig.java
+++ src/main/java/org/tennis/adapter/out/persistence/config/datasource/DataSourceConfig.java
@@ -5,6 +5,10 @@ import jakarta.persistence.PersistenceConfiguration;
 import org.tennis.adapter.out.persistence.entity.MatchEntity;
 import org.tennis.adapter.out.persistence.entity.PlayerEntity;
 
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import static jakarta.persistence.PersistenceConfiguration.JDBC_DRIVER;
 import static jakarta.persistence.PersistenceConfiguration.JDBC_PASSWORD;
 import static jakarta.persistence.PersistenceConfiguration.JDBC_URL;
@@ -13,17 +17,30 @@ import static org.hibernate.cfg.JdbcSettings.SHOW_SQL;
 
 public class DataSourceConfig {
 
+    public static final String HIBERNATE_HBM_2_DDL_AUTO = "hibernate.hbm2ddl.auto";
+    private static final Properties properties = new Properties();
+
+    static {
+        try (InputStream is = DataSourceConfig.class.getClassLoader().getResourceAsStream("hibernate.properties")) {
+            if (is == null) {
+                throw new RuntimeException("Файл hibernate.properties не найден в classpath");
+            }
+            properties.load(is);
+        } catch (IOException e) {
+            throw new RuntimeException("Не удалось загрузить файл hibernate.properties", e);
+        }
+    }
+
     private final EntityManagerFactory emf;
 
-    // TODO: вынести в environment variables
     private DataSourceConfig() {
         final PersistenceConfiguration cfg = new PersistenceConfiguration("emf")
-                .property(JDBC_URL, "jdbc:h2:mem:db1")
-                .property(JDBC_USER, "sa")
-                .property(JDBC_PASSWORD, "")
-                .property(SHOW_SQL, "true")
-                .property(JDBC_DRIVER, "org.h2.Driver")
-                .property("hibernate.hbm2ddl.auto", "drop-and-create")
+                .property(JDBC_DRIVER, properties.getProperty(JDBC_DRIVER))
+                .property(JDBC_URL, properties.getProperty(JDBC_URL))
+                .property(JDBC_USER, properties.getProperty(JDBC_USER))
+                .property(JDBC_PASSWORD, properties.getProperty(JDBC_PASSWORD))
+                .property(SHOW_SQL, properties.getProperty(SHOW_SQL))
+                .property(HIBERNATE_HBM_2_DDL_AUTO,  properties.getProperty(HIBERNATE_HBM_2_DDL_AUTO))
                 .managedClass(PlayerEntity.class)
                 .managedClass(MatchEntity.class);
         emf = cfg.createEntityManagerFactory();
