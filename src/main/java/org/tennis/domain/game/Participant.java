12155c88d92add057a9b0f1cc3c4abd09cc2b874
no-fedov <nefedov.evgeniy.d@yandex.com>
2026-02-03 15:58:04 +0300

    ref: add exception message to Participant

--- src/main/java/org/tennis/domain/game/Participant.java
+++ src/main/java/org/tennis/domain/game/Participant.java
@@ -7,13 +7,15 @@ public enum Participant {
     FIRST("FIRST"),
     SECOND("SECOND");
 
+    public static final String INVALID_PARTICIPANT_ATTRIBUTE_MESSAGE = "Invalid participant attribute: %s";
+
     private final String description;
 
     public static Participant getParticipant(String string) {
         return switch (string) {
             case "FIRST" -> FIRST;
             case "SECOND" -> SECOND;
-            default -> throw new IllegalStateException();
+            default -> throw new IllegalStateException(INVALID_PARTICIPANT_ATTRIBUTE_MESSAGE.formatted(string));
         };
     }
 }
